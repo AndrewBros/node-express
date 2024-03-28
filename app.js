478ffaffb0f4c3acab38004ba37a3d01045426d0
Andrés <67500897+AndrewBros@users.noreply.github.com>
2024-03-28 18:57:45 +0100

    response /

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 let express = require("express");
 let app = express()
 
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
 app.listen(8081, () => {
     console.log("Server in 8081");
 })
